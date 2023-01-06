2c224980b76570e085c37d5824c581c662161982
shailendrahonrao1 <48753187+shailendrahonrao1@users.noreply.github.com>
2023-01-06 12:35:40 +0530

    Updated Controller & View
    Updated the controller folder name and index view

--- WishList/Controllers/HomeController.cs
+++ WishList/Controllers/HomeController.cs
@@ -4,18 +4,18 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace WishList.Controller
+namespace WishList.Controllers
 {
-    public class HomeController : ControllerBase
+    public class HomeController : Controller
     {
         public IActionResult Index()
         {
-            return Index();
+            return View();
         }
 
         public IActionResult Error()
         {
-            return Error();
+            return View();
         }
     }
 }
