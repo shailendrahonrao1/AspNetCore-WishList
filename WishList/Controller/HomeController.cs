708646fb57d883325aceef2357c1d7cae8ed0b8a
shailendrahonrao1 <48753187+shailendrahonrao1@users.noreply.github.com>
2023-01-06 12:20:28 +0530

    Added View & Controller
    Added Index, Error view and controller method in WishList application

---
+++ WishList/Controller/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WishList.Controller
+{
+    public class HomeController : ControllerBase
+    {
+        public IActionResult Index()
+        {
+            return Index();
+        }
+
+        public IActionResult Error()
+        {
+            return Error();
+        }
+    }
+}
